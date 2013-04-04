bc7bdf81bea039471ae91897c9ef8c90b115be29
Jan Tůma <snb.supervec@email.cz>
2013-04-04 22:06:44 +0200

    AHOJ SVETE

---
+++ PrvniKonzolovaAplikace/PrvniKonzolovaAplikace/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrvniKonzolovaAplikace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahoj Světe.");
+            Console.ReadKey();
+        }
+    }
+}

15516f03cb831f21b947a202297b0cd405af9e4f
Jan Tůma <snb.supervec@email.cz>
2013-04-15 17:39:45 +0200

    jk

--- PrvniKonzolovaAplikace/PrvniKonzolovaAplikace/Program.cs
+++ PrvniKonzolovaAplikace/PrvniKonzolovaAplikace/Program.cs
@@ -70,7 +70,7 @@ namespace PrvniKonzolovaAplikace
             Console.WriteLine("Převedl jsem zadaný text na číslo parsováním, zadal jste: " + a);
             Console.ReadKey(); */
 
-
+            /*
             Console.WriteLine("Právě zjistím počet možných pinů ke kreditním kartám...");
             int[] pin = new int[362880];
             for (int i = 0; i <= 9999; i++)
@@ -79,6 +79,89 @@ namespace PrvniKonzolovaAplikace
                 Console.WriteLine(pin[i]);
             }
             Console.ReadKey();
+             */
+
+            /*
+            Console.WriteLine("Vítejte v kalkulačce");
+            bool pokracovat = true;
+            while (pokracovat)
+            {
+                Console.WriteLine("Zadejte první číslo:");
+                float a;
+                while (!float.TryParse(Console.ReadLine(), out a))
+                    Console.WriteLine("Neplatná volba, zadej znovu: ");
+
+                Console.WriteLine("Zadejte druhé číslo:");
+                float b;
+                while (!float.TryParse(Console.ReadLine(), out b))
+                    Console.WriteLine("Neplatná volba, zadej znovu: ");
+
+                Console.WriteLine("Zvolte si operaci:");
+                Console.WriteLine("1 - sčítání");
+                Console.WriteLine("2 - odčítání");
+                Console.WriteLine("3 - násobení");
+                Console.WriteLine("4 - dělení");
+
+                char volba = Console.ReadKey().KeyChar;
+                float vysledek = 0;
+                bool platnaVolba = true;
+
+                switch (volba)
+                {
+                    case '1' :
+                        vysledek = a + b;
+                        break;
+                    case '2' :
+                        vysledek = a - b;
+                        break;
+                    case '3' :
+                        vysledek = a * b;
+                        break;
+                    case '4' :
+                        vysledek = a / b;
+                        break;
+
+                    default :
+                        platnaVolba = false;
+                        break;
+                }
+
+                if (platnaVolba)
+                    Console.WriteLine("\nVýsledek: {0}", vysledek);
+                else
+                    Console.WriteLine("\nNeplatná volba");
+
+                Console.WriteLine("Přejete si zadat další příklad? [a/n]");
+                platnaVolba = false;
+                while (!platnaVolba)
+                {
+                    switch (Console.ReadKey().KeyChar.ToString().ToLower())
+                    {
+                        case "a":
+                            {
+                                pokracovat = true;
+                                platnaVolba = true;
+                                break;
+                            }
+                        case "n":
+                            {
+                                pokracovat = false;
+                                platnaVolba = true;
+                                break;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("\nZadali jste špatnou volbu, eště raz [a/n]: ");
+                                break;
+                            }
+                    }
+                }
+            }
+            Console.WriteLine("\nDěkuji za použití kalkulačky, aplikaci ukončíte libovolnou klávesou.");
+            Console.ReadKey();
+            */
+
+
 
         }
     }

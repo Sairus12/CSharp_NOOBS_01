4db352372dbfc0f4e8b2b073db061580b11669b4
Jan Tůma <snb.supervec@email.cz>
2013-04-15 17:48:02 +0200

    Pokrocile Aplikace

---
+++ PokrocileAplikace/PokrocileAplikace/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokrocileAplikace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] pole;
+            pole = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                pole[i] = i + 1;
+            }
+            foreach (int i in pole)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
